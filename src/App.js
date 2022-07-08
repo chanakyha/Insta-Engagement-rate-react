93352ed003b683decf3153767497e9174de6f61f
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-08 20:04:30 +0530

    The Login Page UI is done ✅

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { AiOutlineInstagram } from "react-icons/ai";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
@@ -5,9 +6,17 @@ import Login from "./pages/Login";
 function App() {
   return (
     <div>
+      <button
+        onClick={() => {
+          window.location.href = "https://www.instagram.com/";
+        }}
+        className="absolute mt-2 ml-2 text-2xl hover:scale-150 ease-in-out duration-300"
+      >
+        <AiOutlineInstagram fill="#fff" />
+      </button>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="about" element={<Login />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,39 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="bg-[#fff] md:rounded-lg p-7 drop-shadow-2xl">
+        <h1 className="text-2xl md:text-3xl text-center font-bold">
+          Login using Instagram Account
+        </h1>
+        <div className="flex flex-col mt-7 m-7 space-y-5">
+          <input
+            placeholder="Instagram Username"
+            className="border px-3 py-2 rounded-md"
+          />
+          <input
+            placeholder="Instagram Password"
+            className="border px-3 py-2 rounded-md"
+            type="password"
+          />
+        </div>
+        <div className="space-y-3 md:space-y-0 md:space-x-2 flex flex-col md:flex-row">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Login
+          </button>
+          <button
+            onClick={() => {
+              window.location.href = "https://www.instagram.com/";
+            }}
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Redirect to Instagram
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
