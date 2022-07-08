3a7ca2ecce6ad805d6e1be08f985ab5413d4bf2c
Chanakyha <vetrichanakyha2003@gmail.com>
2022-07-08 18:03:49 +0530

    React Router Dom is Installed and set up is done ✅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="about" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
